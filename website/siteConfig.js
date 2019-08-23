15277196329fd4270fed1de075f9525d0c76b479
Vo Duy Khanh <khanh-vo@cybozu.vn>
2019-08-23 11:42:29 +0700

    Init for deploy.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -27,7 +27,7 @@ const users = [
 const siteConfig = {
   title: 'kintone UI Component', // Title for your website.
   tagline: 'Documentation for kintone UI Component',
-  url: 'https://phongnhat19.github.io', // Your website URL
+  url: 'https://josh-vo.github.io', // Your website URL
   baseUrl: '/kuc-doc/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -35,7 +35,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'kuc-doc',
-  organizationName: 'phongnhat19',
+  organizationName: 'josh-vo',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
